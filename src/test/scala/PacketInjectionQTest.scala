288862f20beed1b6a0057912d0e4b31f3024a208
Jim Lawson <ucbjrl@berkeley.edu>
2016-04-15 08:49:29 -0700

    Update tests with single test versions.

--- src/test/scala/PacketInjectionQTest.scala
+++ src/test/scala/PacketInjectionQTest.scala
@@ -1,51 +1,6 @@
 package OpenSoC
 
 import Chisel._
-import scala.collection.mutable.HashMap
-import scala.util.Random
-
-class PacketChannel(parms: Parameters) extends Bundle{
-	val packet = new Packet(parms).asInput
-	val packetValid  = Bool(INPUT)
-	val packetReady  = Bool(OUTPUT)
-}
-
-class PacketInjectionQ[T <: Data](parms: Parameters, tGen : Parameters => T) extends Module(parms) {
-
-	val flitizer = parms.get[Parameters=>InputToFlit[T]]("InputFlitizer")
-
-	val io = new Bundle {
-		val in = new ReadyValid[T](parms, tGen)
-		val out = new Channel(parms).flip()
-	}
-	val queueDepth = parms.get[Int]("queueDepth")
-
-	val creditGen = Chisel.Module ( new CreditGen( parms.child("MyGen")) )
-	val creditCon = Chisel.Module ( new CreditCon( parms.child("MyCon", Map(
-		("numCreds"->Soft(queueDepth))))) )
-
-	val queue = Chisel.Module( new Chisel.Queue(new Flit(parms), queueDepth) )
-	//val packet2Flit = Chisel.Module( new PacketToFlit(parms))
-	val packet2Flit = Chisel.Module( flitizer(parms))
-	
-	queue.io.enq.valid 				:= packet2Flit.io.flitValid
-	io.in.packetReady 				:= packet2Flit.io.packetReady
-	creditGen.io.outCredit		 	:= packet2Flit.io.flitValid
-	creditGen.io.inGrant 			:= queue.io.deq.ready
-
-	packet2Flit.io.packet 			:= io.in.packet
-	packet2Flit.io.packetValid 		:= io.in.packetValid	
-	packet2Flit.io.flitReady 		:= creditGen.io.outCredit.grant && queue.io.enq.ready
-	
-	queue.io.enq.bits := packet2Flit.io.flit
-	
-	creditCon.io.inCredit <> io.out.credit
-	creditCon.io.inConsume := queue.io.deq.valid 
-	queue.io.deq.ready := creditCon.io.outCredit
-
-	io.out.flit := queue.io.deq.bits
-	
-}
 
 class PacketInjectionQTest(c: PacketInjectionQ[Packet]) extends Tester(c) {
 
@@ -110,5 +65,3 @@ class PacketInjectionQTest(c: PacketInjectionQ[Packet]) extends Tester(c) {
 	step(10)
 	
 }
-
-
--- src/test/scala/SwitchAllocTest.scala
+++ src/test/scala/SwitchAllocTest.scala
@@ -3,38 +3,6 @@ package OpenSoC
 import Chisel._
 import scala.util.Random
 
-/*
-abstract class AllocatorParams {
-	val AllocatorType = UInt() // Enum
-	val NumReqs = UInt()
-	val NumGrants = UInt()
-	val NumResources = UInt()
-	val NumArbiters = UInt()
-}
-*/
-
-abstract class Allocator(parms: Parameters) extends Module(parms) {
-	val numReqs = parms.get[Int]("numReqs")
-	val numRes = parms.get[Int]("numRes")
-	val arbCtor = parms.get[Parameters=>Arbiter]("arbCtor")
-	val io = new Bundle {
-		val requests = Vec.fill(numRes) { Vec.fill(numReqs){ new RequestIO(parms) }.flip }
-		val resources = Vec.fill(numRes) { new ResourceIO }
-		val chosens = Vec.fill(numRes) { UInt(OUTPUT, Chisel.log2Up(numReqs)) }
-	}
-}
-
-class SwitchAllocator(parms: Parameters) extends Allocator(parms) {	
-	for (i <- 0 until numRes) {
-		val arb = Chisel.Module ( arbCtor(parms.child(("SWArb", i), Map(
-			("numReqs"->Soft(numReqs))
-		))) )
-		arb.io.requests <> io.requests(i)
-		io.resources(i) <> arb.io.resource
-		io.chosens(i) <> arb.io.chosen
-	}
-}
-
 class SwitchAllocTest(c: Allocator) extends Tester(c) {
 	implicit def bool2BigInt(b:Boolean) : BigInt = if (b) 1 else 0
 	
@@ -100,20 +68,3 @@ class SwitchAllocTest(c: Allocator) extends Tester(c) {
 		step(1)
 	}
 }
-
-class VCAllocator(parms: Parameters) extends Allocator(parms) {
-	val numVCs = parms.get[Int]("numVCs")
-	for (i <- 0 until numRes) {
-		val arb = Chisel.Module ( arbCtor(parms.child(("SWArb", i), Map(
-			("numReqs"->Soft(numReqs))
-		))) )
-		arb.io.requests <> io.requests(i)
-		io.resources(i) <> arb.io.resource
-		io.chosens(i) <> arb.io.chosen
-	}
-
-	// FARZAD: In my opinion, based on things discussed, we need two sets allocators here
-	// One allocator will assign an input to an output. This output is actually a set of VCs.
-	// This second allocator will select which VC the input will be assigned to. The best way I can think of at the moment is to have an inverted allocator, one where the requests come from VCs and the resourses are inputs.
-}
-
