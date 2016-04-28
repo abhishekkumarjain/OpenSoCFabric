2089c965b8a137c641d401c0dae1c18373d8311d
Wenyu Tang <danny.tang@a8.Millennium.Berkeley.EDU>
2016-04-28 11:31:00 -0700

    delete redundant copies of testers

--- src/test/scala/BusProbeTest.scala
+++
@@ -1,9 +0,0 @@
-package OpenSoC
-
-import Chisel._
-
-class BusProbeTest(c: BusProbe) extends Tester(c) {
-
-	poke(c.io.inValid(0), 0)
-	step(1)
-}
--- src/test/scala/OpenSoC_CFlatBflyTester.scala
+++
@@ -1,9 +0,0 @@
-package OpenSoC
-
-import Chisel._
-
-/*class OpenSoC_CFlatBflyTester(c: OpenSoC_CFlatBfly) extends Tester(c) {
-	implicit def bool2BigInt(b:Boolean) : BigInt = if (b) 1 else 0
-
-	expect(c.io.ports(0).in.credit.valid, 1)
-}*/
